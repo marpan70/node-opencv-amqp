016726222292f9bab6b5a408f0c7c593185db603
marpan70 <martin@Martins-iMac.local>
2016-03-16 16:21:09 +0100

    file images

--- file.js
+++ file.js
@@ -1,11 +1,36 @@
 var cv = require('opencv');
+var amqp = require('amqp-ts');
 
-cv.readImage("./node_modules/opencv/examples/files/mona.png", function(err, im){
-  im.detectObject(cv.FACE_CASCADE, {}, function(err, faces){
-    for (var i=0;i<faces.length; i++){
-      var x = faces[i]
-      im.ellipse(x.x + x.width/2, x.y + x.height/2, x.width/2, x.height/2);
-    }
-    im.save('./out.jpg');
-  });
-})
+// 192.168.99.100:32779
+// https://github.com/peterbraden/node-opencv
+// https://github.com/abreits/amqp-ts
+
+var connection = new amqp.Connection("amqp://192.168.99.100:32779");
+var exchange = connection.declareExchange("images", 'fanout');
+var queue = connection.declareQueue("opencv");
+queue.bind(exchange);
+queue.activateConsumer((message) => {
+    console.log("Message received: " + message.getContent().length);
+
+    // read image from rmq
+    cv.readImage("/node_modules/opencv/examples/files/mona.png", function(err, im){
+      im.detectObject(cv.FACE_CASCADE, {}, function(err, faces){
+        for (var i=0;i<faces.length; i++){
+          var x = faces[i]
+          im.ellipse(x.x + x.width/2, x.y + x.height/2, x.width/2, x.height/2);
+        }
+
+        // write image to rmq
+        im.save('./out.jpg');
+
+        // exchange.send(message);
+
+      });
+    })
+
+},{rawMessage: true, noAck: true})
+
+// after half a second close the connection
+setTimeout(function() {
+  connection.close();
+}, 1000);
