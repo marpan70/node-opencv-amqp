5f1e72e1191d2fee01558edd8e7609a9667cb475
Marcel Maatkamp <m.maatkamp@gmail.com>
2016-03-17 16:09:16 +0100

    added exchange

--- Dockerfile
+++ Dockerfile
@@ -10,8 +10,9 @@ WORKDIR /home/node/projects/opencv/detector
 ADD detector_opencv_faces.js detector_opencv_faces.js
 
 ENV RABBITMQ_HOST=amqp://rabbitmq:5672
-ENV RABBITMQ_EXCHANGE=images
+ENV RABBITMQ_EXCHANGE_IMAGES=images
+ENV RABBITMQ_EXCHANGE_FACES=faces
 ENV RABBITMQ_EXCHANGE_TYPE=fanout
-ENV RABBITMQ_QUEUE=faces
+ENV RABBITMQ_QUEUE=opencv.face_detector
 
 CMD node detector_opencv_faces.js
--- detector_opencv_faces.js
+++ detector_opencv_faces.js
@@ -1,33 +1,39 @@
 var cv = require('opencv');
 var amqp = require('amqp-ts');
+var fs = require('fs');
 
 // 192.168.99.100:32779
 // https://github.com/peterbraden/node-opencv
 // https://github.com/abreits/amqp-ts
 
 var connection = new amqp.Connection(process.env.RABBITMQ_HOST);
-var exchange = connection.declareExchange(process.env.RABBITMQ_EXCHANGE, process.env.RABBITMQ_EXCHANGE_TYPE);
-var queue = connection.declareQueue(process.env.RABBITMQ_QUEUE);
+var images = connection.declareExchange(process.env.RABBITMQ_EXCHANGE, process.env.RABBITMQ_EXCHANGE_TYPE);
+var faces = connection.declareExchange('faces', process.env.RABBITMQ_EXCHANGE_TYPE);
+var opencv = connection.declareQueue(process.env.RABBITMQ_QUEUE);
 
-queue.bind(exchange);
-queue.activateConsumer((message) => {
-    console.log("Message received: " + message.getContent().length);
+opencv.bind(images);
+opencv.activateConsumer((message) => {
+  console.log("Message received: " + message.content.length);
+  fs.writeFile('/tmp/in.jpg', message.content, 'binary');
 
-    // read image from rmq
-    cv.readImage("/node_modules/opencv/examples/files/mona.png", function(err, im){
-      im.detectObject(cv.FACE_CASCADE, {}, function(err, faces){
-        for (var i=0;i<faces.length; i++){
-          var x = faces[i]
-          im.ellipse(x.x + x.width/2, x.y + x.height/2, x.width/2, x.height/2);
-        }
+  // read image from rmq
+  cv.readImage('/tmp/in.jpg', function(err, im){
+    im.detectObject(cv.FACE_CASCADE, {}, function(err, faces){
+      for (var i=0;i<faces.length; i++){
+        var x = faces[i]
+        im.ellipse(x.x + x.width/2, x.y + x.height/2, x.width/2, x.height/2);
+      }
 
-        // write image to rmq
-        im.save('./out.jpg');
+      // write image to rmq
+      im.save('/tmp/out.jpg');
 
-        // exchange.send(message);
-
+      fs.readFile('/tmp/out.jpg', function read(err, data) {
+        var message = new amqp.Message(data);
+        faces.send(message);
       });
-    })
+
+    });
+  })
 
 },{rawMessage: true, noAck: true})
 
