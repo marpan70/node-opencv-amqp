e023863300ab97387cc68d9d68795af453d88176
Marcel Maatkamp <m.maatkamp@gmail.com>
2016-03-17 17:11:22 +0100

    detector in two steps

--- detector_opencv_faces.js
+++ detector_opencv_faces.js
@@ -14,11 +14,13 @@ var Faces = function (count, faces, data) {
 
 var connection = new amqp.Connection(process.env.RABBITMQ_HOST);
 var images = connection.declareExchange(process.env.RABBITMQ_EXCHANGE_IMAGES, process.env.RABBITMQ_EXCHANGE_TYPE);
-var detected_faces = connection.declareExchange(process.env.RABBITMQ_EXCHANGE_FACES, process.env.RABBITMQ_EXCHANGE_TYPE);
-var opencv = connection.declareQueue(process.env.RABBITMQ_QUEUE);
+var queue_images = connection.declareQueue(process.env.RABBITMQ_QUEUE);
 
-opencv.bind(images);
-opencv.activateConsumer((message) => {
+var detected_faces = connection.declareExchange(process.env.RABBITMQ_EXCHANGE_FACES, process.env.RABBITMQ_EXCHANGE_TYPE);
+var queue_faces = connection.declareQueue('opencv.person_detector');
+
+queue_images.bind(images);
+queue_images.activateConsumer((message) => {
   console.log("Message received: " + message.content.length);
   // fs.writeFile('/tmp/in.jpg', message.content, 'binary');
 
@@ -29,21 +31,35 @@ opencv.activateConsumer((message) => {
     im.detectObject(cv.FACE_CASCADE, {}, function(err, faces){
       console.log(faces);
       if(typeof faces != 'undefined' && faces) {
-
-        for (var i=0;i<faces.length; i++){
-          var x = faces[i]
-          im.ellipse(x.x + x.width/2, x.y + x.height/2, x.width/2, x.height/2);
-        }
-        im.save('/tmp/out.jpg');
-        fs.readFile('/tmp/out.jpg', function read(err, data) {
-          var facesObject = new Faces(faces.length, faces, message.content.toString('base64'));
-          detected_faces.send(new amqp.Message(facesObject));
-        });
+        var facesObject = new Faces(faces.length, faces, message.content.toString('base64'));
+        detected_faces.send(new amqp.Message(facesObject));
       }
     });
   })
 
 },{rawMessage: true, noAck: true})
+
+queue_faces.bind(detected_faces);
+queue_faces.activateConsumer((message) => {
+  console.log("faces received: " + message.content.length);
+  var facesObject = Object.create(Faces, message.getContent());
+  console.log("faces received: " +  facesObject._count);
+  if(typeof faces != 'undefined' && faces) {
+    fs.writeFile('/tmp/in.jpg', message.content, 'binary');
+
+    for (var i=0;i<faces.length; i++){
+      var x = faces[i]
+      im.ellipse(x.x + x.width/2, x.y + x.height/2, x.width/2, x.height/2);
+    }
+    im.save('/tmp/out.jpg');
+    fs.readFile('/tmp/out.jpg', function read(err, data) {
+      var facesObject = new Faces(faces.length, faces, message.content.toString('base64'));
+      detected_faces.send(new amqp.Message(facesObject));
+    });
+  }
+
+},{rawMessage: true, noAck: true})
+
 
 // after half a second close the connection
 setTimeout(function() {
